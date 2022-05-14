86fe42bd782595129aa553d11d70b82c58100d12
Rageicans <patrickryanyoung86@gmail.com>
2022-05-14 14:28:46 +0800

    Updated

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <header>
-<h1>💬</h1>
+<h1>🔫💬</h1>
   {#if $username}
     <div class="user-bio">
       
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Chat</h3>
+      <h3>Gun.js Chat</h3>
   {/if}
 </header>
